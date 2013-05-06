054fb8ec3f178bf7d1f8ede742c28c7faabca53b
PRBO Dev <devprbo@pointblue.org>
2013-05-06 22:22:43 +0000

    add google analytics

--- iwjvmap.php
+++ iwjvmap.php
@@ -24,7 +24,19 @@
         <?php endif;?>
             
         <link type="text/css" rel="stylesheet" href="inc/css/iwjvmap.css<?php echo $__VERSION ?>">
-        
+        <script type="text/javascript">
+
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-40690497-30']);
+          _gaq.push(['_trackPageview']);
+
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+
+        </script>
         <script type="text/javascript" src="inc/js/ext-3.4.0/adapter/ext/ext-base.js"></script>
         <script type="text/javascript" src="inc/js/ext-3.4.0/ext-all.js"></script>
         <script type="text/javascript" src="http://maps.google.com/maps?file=api&amp;v=3&amp;key=AIzaSyDs6QcNHclTKt5HZNacqC6Cfn2f7JGIXjo"></script>
