65ea649dda56410d879e8112ae07d0247c51ecc3
Martin Magana <magana.web@gmail.com>
2015-07-30 12:57:06 -0700

    core css updates
    css for the core components has been updated so that the page doesn't look so bad

--- app/habpop.controller.js
+++ app/habpop.controller.js
@@ -20,25 +20,32 @@
 
         HabpopMap.initialize();
 
-
         function handlePointDropped(geom){
             //reset the worksheet form
             console.log('point dropped handler called from habpop controller');
             StategonRequest.get(geom)
-                .then(loadStategon);
+                .then(loadStategon)
+                .then(resetWorksheetModels)
+                .then(resetEstimate);
+        }
+
+        function resetWorksheetModels(){
+            vm.worksheetModels = {};
+        }
+
+        function resetEstimate(){
+            vm.estimate = {'before':{},'after':{}};
         }
 
         function loadStategon(stategon){
             $log.debug('loading stategon:', stategon);
             vm.stategon = stategon;
-
         }
 
         function submitWorksheets(){
             $log.debug('submitting worksheets', vm.worksheetModels);
             EstimatesRequest.get(vm.worksheetModels)
                 .then(loadEstimate);
-
         }
 
         function loadEstimate(estimate){
