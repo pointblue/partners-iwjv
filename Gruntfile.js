f437cb4ecedc1309ad5c1470ccc7f36bb3d2972a
Martin Magana <magana.web@gmail.com>
2016-03-29 15:49:37 -0700

    Update old dependency, and href
    The link title 'Sage obligates and the IWJV' which refered to http://iwjv.org has been changed and now refers to http://iwjv.org/sagebrush-landscape-planning

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function(grunt){
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-concat-sourcemaps');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
